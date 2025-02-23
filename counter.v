ae2093143ceb6930f53127091fa2d9e164a9afd0
andreaasprer <140675071+andreaasprer@users.noreply.github.com>
2025-02-22 22:07:14 -0800

    add files

---
+++ counter.v
@@ -0,0 +1,98 @@
+`timescale 1ns / 1ps
+
+module counter(
+    // INPUTS
+	input clk_1hz,
+	input clk_2hz,
+	input sel,
+	input adj,
+	input rst,
+	input pause,
+	// Outputs
+	output reg [3:0] minutes_tens,
+	output reg [3:0] minutes_ones,
+	output reg [3:0] seconds_tens,
+	output reg [3:0] seconds_ones
+);
+	
+	// Get constants
+	`include "stopwatch_constants.v"
+	
+	// Handle pause state
+	reg is_paused = 0;
+	
+	sel_adj adjust(.adj(adj), .clk_1hz(clk_1hz), .clk_2hz(clk_2hz), .which_clk(clk));
+	
+	always @(posedge pause) begin
+		// Pause the clock
+		is_paused = ~is_paused;
+	end
+	
+	// Fast clock increase
+	always @(posedge clk or posedge rst) begin
+		if (rst) begin
+			// Reset clock
+			minutes_tens <= 0;
+			minutes_ones <= 0;
+			seconds_tens <= 0;
+			seconds_ones <= 0;
+		end else if (adj && ~is_paused) begin
+			// Make the minutes update faster
+			if (~sel) begin
+				// Update 1s place
+				if (minutes_ones == MAX_ONES) begin
+					minutes_ones <= 0;
+					// Update 10s place
+					if (minutes_tens == MAX_TENS) begin
+						minutes_tens <= 0;
+					end else begin
+						minutes_tens <= minutes_tens + 1'b1;
+					end
+				end else begin
+					minutes_ones <= minutes_ones + 1'b1;
+				end
+			// Make the seconds update faster
+			end else begin
+				// Update 1s place
+				if (seconds_ones == MAX_ONES) begin
+					seconds_ones <= 0;
+					// Update 10s place
+					if (seconds_tens == MAX_TENS) begin
+						seconds_tens <= 0;
+					end else begin
+						seconds_tens <= seconds_tens + 1'b1;
+					end
+				end else begin
+					seconds_ones <= seconds_ones + 1'b1;
+				end
+			end
+		// Regular clock
+		end else if (~adj && ~is_paused) begin
+			// Update seconds (1s place)
+			if (seconds_ones == MAX_ONES) begin
+				seconds_ones <= 0;
+				// Update seconds (10s place)
+				if (seconds_tens == MAX_TENS) begin
+					seconds_tens <= 0;
+					// Update minutes (1s place)
+					if (minutes_ones == MAX_ONES) begin
+						minutes_ones <= 0;
+						// Update minutes (10s place)
+						if (minutes_tens == MAX_TENS) begin
+							minutes_tens <= 0;
+						end else begin
+							minutes_tens <= minutes_tens + 1'b1;
+						end
+					end else begin
+						minutes_ones <= minutes_ones + 1'b1;
+					end
+				end else begin
+					seconds_tens <= seconds_tens + 1'b1;
+				end
+			end else begin
+				seconds_ones <= seconds_ones + 1'b1;
+			end
+		end
+	end
+	
+endmodule
---
+++ stopwatch_tb.v
@@ -0,0 +1,165 @@
+`timescale 1ns / 1ps
+
+module stopwatch_tb;
+
+	// Inputs
+	reg clk;
+	reg SEL;
+	reg ADJ;
+	reg RESET;
+	reg PAUSE;
+
+	// Outputs
+	wire [6:0] cathode;
+	wire [3:0] anode;
+
+	// Instantiate the Unit Under Test (UUT)
+	stopwatch uut (
+		.clk(clk), 
+		.SEL(SEL), 
+		.ADJ(ADJ), 
+		.RESET(RESET), 
+		.PAUSE(PAUSE), 
+		.cathode(cathode), 
+		.anode(anode)
+	);
+	
+	// Global clock
+	always #5 clk = ~clk;
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		SEL = 0;
+		ADJ = 0;
+		RESET = 0;
+		PAUSE = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Reset
+		RESET = 1;
+		// Wait 1ms for the debouncer to allow the signal through
+		#1100000;
+		RESET = 0;
+		#1100000;
+		
+		$display("Reset complete, beginning stopwatch");
+		
+		// Test one second default
+		#500000000;
+		
+		$display("-------------------------------------------");
+		$display("Normal mode test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		
+		// Test ADJ, SEL = 0
+		ADJ = 1;
+		#1100000;
+		#500000000;
+		
+		$display("-------------------------------------------");
+		$display("Minute adjustment mode test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		
+		// Test ADJ, SEL = 1
+		SEL = 1;
+		#1100000;
+		#500000000;
+		
+		$display("-------------------------------------------");
+		$display("Second adjustment mode test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		
+		// Test ADJ = 0
+		ADJ = 0;
+		#1100000;
+		#1000000000;
+		
+		$display("-------------------------------------------");
+		$display("ADJ off mode test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		
+		// Test PAUSE
+		PAUSE = 1;
+		#1100000;
+		#100000000;
+		PAUSE = 0;
+		#1100000;
+		
+		$display("-------------------------------------------");
+		$display("Pause test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		
+		// Test Unpause
+		PAUSE = 1;
+		#1100000;
+		#100000000;
+		PAUSE = 0;
+		#1100000;
+		
+		$display("-------------------------------------------");
+		$display("Unpause test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		
+		// Test RESET
+		RESET = 1;
+		#1100000;
+		
+		$display("-------------------------------------------");
+		$display("Reset test complete\nResults:");
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+		$display("%b: %b", anode, cathode);
+		#4000000;
+
+	end
+      
+endmodule
